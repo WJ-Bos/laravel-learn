53216c591c2d1dd3807b6d01b1699888bd0e3c0b
WJbos <wjbosdev@gmail.com>
2025-03-25 20:39:10 +0200

    Added Logout and Dasboard Route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\AuthController;
 Route::view('/', 'posts.index')->name('home');
 Route::view('/register', 'auth.register')->name('register');
 Route::view('/login','auth.login')->name('login');
+Route::view('/dashboard','users.dashboard')->name('dashboard');
 
 Route::post('/register', [AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
