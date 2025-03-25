7a2d8a44ad1dbd8a302b68104a34a0cc057ed7fb
WJbos <wjbosdev@gmail.com>
2025-03-25 20:39:27 +0200

    fixed login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,17 +6,18 @@
             <h2 class="text-2xl font-bold text-gray-800 text-center">Welcome back</h2>
            @error('failed')
                  <p class="text-xs text-red-500 font-light mt-1">{{ $message }}</p>
-           @enderror    
+           @enderror
             <div>
                 <label for="email" class="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                <input type="text" id="email"
+                <input type="text" id="email" name="email"
+                    value="{{ old('email') }}"
                     class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
                     placeholder="your@email.com">
             </div>
 
             <div>
                 <label for="password" class="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                <input type="password" id="password"
+                <input type="password" id="password" name="password"
                     class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
                     placeholder="••••••••">
             </div>
@@ -26,6 +27,7 @@
             </div>
 
             <button
+                type="submit"
                 class="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                 Login
             </button>
