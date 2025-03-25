86be9eca074f4780c0f4da87a07b548abfcdccde
WJbos <wjbosdev@gmail.com>
2025-03-25 20:39:17 +0200

    Added Logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,9 +10,10 @@ use Illuminate\Support\Facades\Redirect;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $fields=$request->validate([
-            'name'=>'required|max:255|string',
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|max:255|string',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:3|confirmed'
         ]);
@@ -23,15 +24,32 @@ class AuthController extends Controller
         return redirect()->route('home');
     }
 
-    public function login(Request $request){
-       $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
         ]);
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
-            return redirect()->intended(route('home'));
+            return redirect()->intended();
         }
+
+        return back()->withErrors([
+            'failed' => 'Provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+       $request->session()->invalidate();
+
+       $request->session()->regenerateToken();
+
+       return redirect('/');
     }
 }
